018ad21dbcbb5b2e297937e092a31678a00fe024
86 <triaedz@gmail.com>
2015-03-30 02:52:05 +0900

    implemnt

--- index.ios.js
+++ index.ios.js
@@ -7,11 +7,14 @@
 var React = require('react-native');
 var {
   AppRegistry,
-  Image,
-  ListView,
   StyleSheet,
-  Text,
   View,
+  ListView,
+  Image,
+  Text,
+  NavigatorIOS,
+  WebView,
+  TouchableHighlight
 } = React;
 
 var API_URL = 'https://api.github.com/search/repositories?q=react+native&sort=stars&order=desc';
@@ -19,6 +22,20 @@ var PARAMS = '';
 var REQUEST_URL = API_URL + PARAMS;
 
 var ReactNativeStars = React.createClass({
+  render: function() {
+    return (
+      <NavigatorIOS
+        style={styles.navigator}
+        initialRoute={{
+          component: ItemList,
+          title: 'React Native Stars'
+          }}
+      />
+    );
+  }
+});
+
+var ItemList = React.createClass({
   getInitialState: function() {
     return {
       dataSource: new ListView.DataSource({
@@ -69,47 +86,89 @@ var ReactNativeStars = React.createClass({
   },
 
   renderItem: function(item) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: item.owner.avatar_url}}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{item.name}</Text>
-          <Text style={styles.year}>{item.stargazers_count}</Text>
-        </View>
-      </View>
+    return(
+      <ItemCell
+        onSelect={() => this.selectItem(item)}
+        item={item}
+      />
     );
+  },
+
+  selectItem: function(item) {
+    this.props.navigator.push({
+      title: item.full_name,
+      component: WebView,
+      passProps: {url: item.html_url}
+    });
   },
 });
 
+var ItemCell = React.createClass({
+  render: function() {
+    return (
+      <TouchableHighlight onPress={this.props.onSelect}>
+      <View style={styles.container}>
+        <Image
+          source={{uri: this.props.item.owner.avatar_url}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{this.props.item.name}</Text>
+          <Text style={styles.owner}>{this.props.item.owner.login}</Text>
+          <Text style={styles.stars}>☆{this.props.item.stargazers_count}</Text>
+        </View>
+      </View>
+      </TouchableHighlight>
+    );
+  }
+});
+
 var styles = StyleSheet.create({
+  navigator: {
+    flex: 1,
+  },
+  listView: {
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#FFFFFF',
+  },
   container: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: '#FFFFFF',
   },
   rightContainer: {
     flex: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
   title: {
-    fontSize: 16,
-    marginBottom: 8,
+    fontSize: 18,
     textAlign: 'left',
+    color: '#444444',
   },
-  year: {
+  owner: {
+    fontSize: 15,
     textAlign: 'left',
+    color: '#888888',
+  },
+  stars: {
+    fontSize: 20,
+    textAlign: 'right',
+    color: '#444444',
   },
   thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
   },
 });
 
